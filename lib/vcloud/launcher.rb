0877edb3fa0c661b5be7cf8eb2dd2412f380af2a
Andrew Hodgson <ah9@me.com>
2014-10-03 10:33:34 +0100

    Add schema for vapp network

--- lib/vcloud/launcher.rb
+++ lib/vcloud/launcher.rb
@@ -1,6 +1,7 @@
 require 'vcloud/core'
 
 require 'vcloud/launcher/cli'
+require 'vcloud/launcher/schema/vapp_network'
 require 'vcloud/launcher/schema/vm'
 require 'vcloud/launcher/schema/vapp'
 require 'vcloud/launcher/schema/launcher_vapps'
